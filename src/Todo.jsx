988e84c28b8d91317f4166fddd35f54424f11259
odgr-show <121861788+odgr-show@users.noreply.github.com>
2024-01-08 22:06:36 +0900

    入力フォームに入力した文字をliリストのpタグに反映させて、新たにliリストを追加する設定を行いました。

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,43 +1,59 @@
+import React from 'react';
 import './stylesheet/style.css';
 
 // ネームドエクスポート
 export const Todo = () => {
+    const [todoText, setTodoText] = React.useState('');
+    const [incompleteTodos, setIncompleteTodos] = React.useState(['Todoです1', 'Todoです2']);
+    const [completeTodos, setCompleteTodos] = React.useState(['Todoでした']);
+
+    // inputフォームに文字が打てるようにする処理
+    const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+    // 追加ボタンを押すとliの要素が追加される処理
+    const onClickAdd = () => {
+        if (todoText === '') return;
+        const newTodos = [...incompleteTodos, todoText];
+        setIncompleteTodos(newTodos);
+        setTodoText('');
+    };
+
     return (
         <>
             <div className="input-area">
-                <input className="add-text" placeholder="TODOを入力"></input>
-                <button>追加</button>
+                <input
+                    className="add-text"
+                    placeholder="TODOを入力"
+                    value={todoText}
+                    onChange={onChangeTodoText}
+                />
+                <button onClick={onClickAdd}>追加</button>
             </div>
             <section className="incomplete-area">
                 <p className="section-title">未完了のTODO</p>
-                <ul className="incomplete-list">
-                    <li className="list-row">
-                        <p>TODOです</p>
-                        <div>
-                            <button>完了</button>
-                            <button>削除</button>
-                        </div>
-                    </li>
-                    <li className="list-row">
-                        <p>TODOです</p>
-                        <div>
-                            <button>完了</button>
-                            <button>削除</button>
-                        </div>
-                    </li>
+                <ul>
+                    {incompleteTodos.map((todo) => (
+                        <li key={todo}>
+                            <div className="list-row">
+                                <p>{todo}</p>
+                                <button>完了</button>
+                                <button>削除</button>
+                            </div>
+                        </li>
+                    ))}
                 </ul>
             </section>
             <section className="complete-area">
                 <p className="section-title">完了のTODO</p>
-                <ul className="complete-list">
-                    <li className="list-row">
-                        <p>TODOでした</p>
-                        <button>戻す</button>
-                    </li>
-                    <li className="list-row">
-                        <p>TODOでした</p>
-                        <button>戻す</button>
-                    </li>
+                <ul>
+                    {completeTodos.map((todo) => (
+                        <li key={todo}>
+                            <div className="list-row">
+                                <p>{todo}</p>
+                                <button>戻す</button>
+                            </div>
+                        </li>
+                    ))}
                 </ul>
             </section>
         </>
